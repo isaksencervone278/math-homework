c608ed507a9cf40ee2ad8045b2fbec7377bc1583
isaksencervone278 <isaksencervone278@gmail.com>
2025-03-05 22:38:00 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,9 @@
+ <?php
+   function getRandomMathHomework() {
+     // Generate a random number between 1 and 10
+     $randomNumber = rand(1, 10);
+
+     // Print out the problem
+     echo "What is $randomNumber x 2?";
+   }
+ ?>
