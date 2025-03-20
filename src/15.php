0e8c5529122e28ebb6605837ce8dd56b80ce53a3
isaksencervone278 <isaksencervone278@gmail.com>
2025-03-20 21:23:16 +0000

    Add 15.php

---
+++ src/15.php
@@ -0,0 +1,18 @@
+<?php
+// Example PHP code for your weekly math homework assignment
+
+/*
+ * Please note that this is just an example and should not be used as-is.
+ * This is provided solely to showcase the format and help you get started.
+ */
+
+// Define a variable with some numbers
+$x = 3;
+$y = 7;
+$z = 2;
+
+// Perform calculations
+$result = $x + $y - $z;
+echo "The result of $x + $y - $z is: $result";
+
+?>
