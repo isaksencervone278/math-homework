2501d6d52ea097a45023749f5e479b6b6b4bcf49
isaksencervone278 <isaksencervone278@gmail.com>
2025-04-04 05:33:48 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,10 @@
+<?php
+// Define variables and constants from math_homework.php
+$variable1 = 5; // Example value
+$constant1 = 2;
+const variable2 = "example";
+
+// Use $constant1 in the code
+
+// Example output:
+echo "The result of $variable1 plus $constant1 is: $constant1 + $variable1\n";
